4bdefe2f78327106bcbd6ef9be7e8760312a00bb
Dhanush <dhanushsubbia703@gmail.com>
2025-09-17 11:20:49 +0530

    feat: ci pipelines

--- database/main.py
+++ database/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker,create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
-DB_URL = os.getenv("DB_URL")
+DB_URL = os.environ.get("DB_URL")
 ENGINE = create_async_engine(DB_URL)
 SessionLocal = async_sessionmaker(ENGINE)
 BASE = declarative_base()
