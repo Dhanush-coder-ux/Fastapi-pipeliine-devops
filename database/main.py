b50f44dfd82ab21810acdbcd5d8af722b0594c01
Dhanush <dhanushsubbia703@gmail.com>
2025-09-17 11:18:28 +0530

    feat: ci pipelines

--- database/main.py
+++ database/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker,create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
-DB_URL = os.getenv("DB_URL","postgresql+asyncpg://postgres:password@localhost/fastapi_db")
+DB_URL = os.getenv("DB_URL")
 ENGINE = create_async_engine(DB_URL)
 SessionLocal = async_sessionmaker(ENGINE)
 BASE = declarative_base()
